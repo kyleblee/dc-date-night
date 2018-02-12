7decc697c782b7eaeeca0827ed3a9eac06403486
kyleblee <hello.kylelee@gmail.com>
2018-02-12 16:49:38 -0500

    Create NavBar.js in components directory and insert component into App.js. Add header container that will encompass logo/slogan, navbar, and sign-in/sign-out. Include NavBar inside new Header container. Add dummy styles to header to start seeing layout changes.

---
+++ client/src/components/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NavBar = props => {
+  return (
+    <nav>
+    </nav>
+  )
+}
+
+export default NavBar;
---
+++ client/src/containers/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/navbar';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className='header'>
+        <NavBar />
+      </div>
+    )
+  }
+}
+
+export default Header;
