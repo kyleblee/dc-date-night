b4636c6ba4c04a534629cc9b726b80d11196cd63
kyleblee <hello.kylelee@gmail.com>
2018-02-13 13:17:46 -0500

    Add some seed data to the database so there is something to play around with when creating JSON API. Add spot attribute explanation to DateEntry model. Change required validation of DateEntry.expert property to false on model.

--- app/models/date_entry.rb
+++ app/models/date_entry.rb
@@ -1,6 +1,7 @@
 class DateEntry < ApplicationRecord
-  belongs_to :expert
+  belongs_to :expert, required: false
   belongs_to :neighborhood
 
   validates :title, :spots, :neighborhood_id, presence: true
+  #  The spot property is expected to be a string of spot_ids separated by spaces
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create first date information
+new_user = User.create(name: "Michael Scott", email: "hello@michaelscott.com", password: "fakepw")
+brewery = Category.create(name: "Brewery")
+neighborhood = Neighborhood.create(name: "Shaw")
+right_proper = Spot.create(name: "Right Proper Brewing Company", category_id: 1, neighborhood_id: 1)
+date1 = DateEntry.create(title: "Brewery Hops in Shaw", spots: "1", neighborhood_id: 1)
