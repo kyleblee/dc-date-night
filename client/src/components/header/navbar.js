346b49786c4dd07a318d6d2ea7cad2c76b092b80
kyleblee <hello.kylelee@gmail.com>
2018-02-14 22:28:45 -0500

    Make homepage directory in components directory. Create QuickAbout component file and define the component.

--- client/src/components/header/navbar.js
+++ client/src/components/header/navbar.js
@@ -4,7 +4,7 @@ const NavBar = props => {
   return (
     <nav className='navbar'>
       <a href='#'>Create</a>
-      <a href='#'>Expert Picks</a>
+      <a href='#'>Browse</a>
       <a href='#'>Share</a>
       <a href='#'>About</a>
     </nav>
