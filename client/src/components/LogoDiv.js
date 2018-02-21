817776898f613b16ae7f941ca8452ce157e47598
kyleblee <hello.kylelee@gmail.com>
2018-02-20 22:59:08 -0500

    Brining CurateDateForm elements together to emphasize groups. Try new logotype, with line break between DC and Date Night to make header look less lopsided.

--- client/src/components/LogoDiv.js
+++ client/src/components/LogoDiv.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const LogoDiv = props => {
   return (
     <div className="logo-div">
-      <Link to='/'><h2 id="logo">DC Date Night</h2></Link>
+      <Link to='/'><h2 id="logo">DC <br/> Date Night</h2></Link>
     </div>
   )
 }
