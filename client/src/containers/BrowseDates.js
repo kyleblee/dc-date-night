957a14a24c1c2500ec8a7e8073484113b3044a6f
kyleblee <hello.kylelee@gmail.com>
2018-02-21 17:55:17 -0500

    Import NeighborhoodSelect into BrowseDates container. Pass in labelText prop to NeighborhoodSelect. Add constructor with initial state to BrowseDates container. Add mapDispatchToProps and mapStateToProps to BrowseDates. Pass in neighborhoods to NeighborhoodSelect.

--- client/src/containers/BrowseDates.js
+++ client/src/containers/BrowseDates.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { NeighborhoodSelect } from '../components/NeighborhoodSelect';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { generateDate, collectNeighborhoodOptions, collectCategoryOptions } from '../actions/dateActions';
 
 class BrowseDates extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {}
+  }
+
+  componentDidMount() {
+    this.props.collectNeighborhoodOptions();
+  }
+
   render() {
     return (
-      <p>Congrats, you've reached the BrowseDates view.</p>
+      <div className="browse-dates">
+        <NeighborhoodSelect
+          labelText="Choose a neighborhood to filter results."
+          neighborhoods={this.props.neighborhoods}/>
+      </div>
     )
   }
 }
 
-export default BrowseDates;
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    collectNeighborhoodOptions: collectNeighborhoodOptions
+  }, dispatch);
+}
+
+const mapStateToProps = (state) => {
+  return {
+    neighborhoods: state.dates.options.neighborhoods
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BrowseDates);
