4494c26c977bf90c9ef84e5c2358b74533f897f3
kyleblee <hello.kylelee@gmail.com>
2018-02-23 22:28:58 -0500

    Add initial form elements and initial state for SignIn. Connect input elements and state for controlled elements in SignIn. Add padding to input[type=‘text’] and textarea elements. Apply universal label / input styles to SignIn.

--- client/src/containers/SignIn.js
+++ client/src/containers/SignIn.js
@@ -3,9 +3,41 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 class SignIn extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  updateInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
     return (
-      <p>You made it to the sign in page.</p>
+      <div id="sign-in-div">
+        <form id="sign-in-form">
+          <div>
+            <label className="form-labels">Email:</label>
+            <input
+              name="email"
+              type="text"
+              value={this.state.email}
+              onChange={this.updateInput} /><br />
+          </div>
+          <label className="form-labels">Password:</label>
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.updateInput} />
+        </form>
+      </div>
     )
   }
 }
